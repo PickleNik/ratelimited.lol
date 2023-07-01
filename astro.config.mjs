97726a62e2fc552f81047261dd5d9268766f01eb
Nikita Krupin <krupin.nikita0110@gmail.com>
2023-07-01 17:52:23 -0400

    pages prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://picklenik.github.io",
+  base: "/ratelimited.lol",
 });
